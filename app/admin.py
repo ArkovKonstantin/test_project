9f82b02022b72250c998ec1a461f7ba123faf1ff
konstantin <arkov.constantin@yandex.ru>
2018-07-29 22:11:30 +0300

    register models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Point)
+admin.site.register(PointType)
